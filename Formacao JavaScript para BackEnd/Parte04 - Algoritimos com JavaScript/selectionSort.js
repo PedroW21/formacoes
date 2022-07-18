b21df2e52fde9af80200a482dc1af87ef94df081
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-07-18 14:36:37 -0300

    Rescrevendo usando forEach no lugar do for tradicional

--- Formacao JavaScript para BackEnd/Parte04 - Algoritimos com JavaScript/selectionSort.js
+++ Formacao JavaScript para BackEnd/Parte04 - Algoritimos com JavaScript/selectionSort.js
@@ -15,4 +15,18 @@ for (let atual = 0; atual < livros.length - 1; atual++)
     livros[menor] = livroAtual;
 }
 
+// Escrevendo o loop acima com forEach
+/*
+
+livros.forEach((_, indice) => { // o " _ " foi utilizado como uma notação que informa que o primeiro argumento nao sera utilizado (originalmente ali seria o elemento)
+    let menor = menorValor(livros, atual);
+    
+    let livroAtual = livros[atual];
+    let livroMenorPreco = livros[menor];
+
+    livros[atual] = livroMenorPreco;
+    livros[menor] = livroAtual;
+})
+
+*/
 console.log(livros);
