c08287d13935b455ec0afc57c6bae4e65d17292b
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-08-04 14:55:07 -0300

    Brincando com a lib chalk e importanto o ultimo arquivo base

--- Formacao JavaScript para BackEnd/Parte05 - NodeJS e JavaScript Orientado a Objetos/NodeJS - Criando sua biblioteca/index.js
+++ Formacao JavaScript para BackEnd/Parte05 - NodeJS e JavaScript Orientado a Objetos/NodeJS - Criando sua biblioteca/index.js
@@ -1,4 +1,11 @@
-console.log("Começando :D");
+import chalk from 'chalk';
 
+console.log(chalk.blue("Começando! Olá mundo! :D"));
+
+console.log(`
+CPU: ${chalk.hex("#FF4500")("86%")} usage!
+RAM: ${chalk.blue("34%")} usage!
+GPU: ${chalk.rgb(91, 10, 145).bold("96%")} usage! ${chalk.hex("#E3CE12").bold("Are you crazy??")}
+`);
 
 
