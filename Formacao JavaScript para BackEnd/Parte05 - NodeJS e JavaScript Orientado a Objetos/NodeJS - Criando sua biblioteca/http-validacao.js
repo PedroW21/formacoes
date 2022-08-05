2434100b5930c4c8183a450a4317730a30dad4a4
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-08-05 11:15:41 -0300

    Finalizado a testagem do link, e a recomposição do objeto mostrado na CLI, partindo para mensagens de erro melhores

--- Formacao JavaScript para BackEnd/Parte05 - NodeJS e JavaScript Orientado a Objetos/NodeJS - Criando sua biblioteca/http-validacao.js
+++ Formacao JavaScript para BackEnd/Parte05 - NodeJS e JavaScript Orientado a Objetos/NodeJS - Criando sua biblioteca/http-validacao.js
@@ -1,17 +1,33 @@
 import fetch from "node-fetch";
 
+function manejarErros(erro)
+{
+    throw new Error(erro.message);
+}
+
 async function checarStatus(arrayDeURLs)
 {
-    const arrayStatus = await Promise.all(arrayDeURLs.map(async url => {
-        const res = await fetch(url); // res = response
-        return res.status;
-    }));
-    return arrayStatus;
+    try
+    {
+        const arrayStatus = await Promise
+        .all(arrayDeURLs
+            .map(async url => {
+                const res = await fetch(url); // res = response
+                return res.status;
+        }));
+        return arrayStatus;
+    }
+    catch(erro)
+    {
+        manejarErros(erro);
+    }
 }
 
 function gerarArrayDeURLs(arrayDeLinks)
 {
-    return arrayDeLinks.map(objetoLink => Object.values(objetoLink).join()); // sempre retorna um array o map :D
+    return arrayDeLinks
+    .map(objetoLink => Object
+        .values(objetoLink).join()); // sempre retorna um array o map :D
 }
 
 async function validarURLs(arrayDeLinks)
@@ -19,7 +35,12 @@ async function validarURLs(arrayDeLinks)
     const links = gerarArrayDeURLs(arrayDeLinks);
     const statusLinks = await checarStatus(links);
 
-    return statusLinks;
+    const resultados = arrayDeLinks.map( (objetoLink, indice) => ({
+        ...objetoLink, 
+        status: statusLinks[indice]
+    })) // envolvemos o obj com parenteses pois queremos q a callback retorne um obj
+
+    return resultados;
 }
 
 export default validarURLs;
